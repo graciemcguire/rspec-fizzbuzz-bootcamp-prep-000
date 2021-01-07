476efbdfcd8b860a25bb5c680670c33d0e937627
Gracie McGuire <gracie.mcguire@flatironschool.com>
2021-01-07 22:47:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,24 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num)
+# def fizzbuzz(num)
    
-   if int % 3 == 0
-   "Fizz"
-   elsif int % 5 == 0
-   "Buzz"
-   elsif int % 15 == 0
-   "FizzBuzz"
+#   if int % 3 == 0
+#   "Fizz"
+#   elsif int % 5 == 0
+#   "Buzz"
+#   elsif int % 15 == 0
+#   "FizzBuzz"
+#   end
+# end
+
+def fizzbuzz(num)
+  if num % 5 == 0 && num % 3 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0 
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else 
+    return nil
   end
-end
+end   
